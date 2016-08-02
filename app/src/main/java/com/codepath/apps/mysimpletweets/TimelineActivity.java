016dc36ad01d4b4210aaa964e2bb680af4467061
zhiyong tan <zhiyong@uber.com>
2016-08-02 11:53:23 -0700

    Walkthrough video

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -0,0 +1,61 @@
+package com.codepath.apps.mysimpletweets;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    private TwitterClient client;
+    private ArrayList<Tweet> tweets;
+    private TweetsArrayAdapter aTweets;
+    private ListView lvTweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        // Find the listview
+        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        // Create the arraylist (data source)
+        tweets = new ArrayList<>();
+        // Construct the adapter from data source
+        aTweets = new TweetsArrayAdapter(this, tweets);
+        // Connect adapter to list view
+        lvTweets.setAdapter(aTweets);
+        // Get the client
+        client = TwitterApplication.getRestClient();    // singleton client
+        populateTimeline();
+    }
+
+    // Send an API request to get the timeline json
+    // Fill the listview by creating the tweet objects from the json
+    private void populateTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
+                Log.d("DEBUG", json.toString());
+                // JSON HERE
+                // DESERIALIZE JSON
+                // CREATE MODELS AND ADD THEM TO THE ADAPTER
+                // LOAD THE MODEL DATA INTO LISTVIEW
+                aTweets.addAll(Tweet.fromJSONArray(json));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/models/Tweet.java
@@ -0,0 +1,67 @@
+package com.codepath.apps.mysimpletweets.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+// Parse the JSON + Store the data, encapsulate state logic or display logic
+public class Tweet {
+    // list out the attributes
+    private String body;
+    private long uid;   // unique id for the tweet
+    private User user;  // store embedded user object
+    private String createdAt;
+
+    public String getBody() {
+        return body;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    // Deserialize the JSON
+    public static Tweet fromJSON (JSONObject jsonObject) {
+        Tweet tweet = new Tweet();
+        // Extract the values from the JSON, store them
+        try {
+            tweet.body = jsonObject.getString("text");
+            tweet.uid = jsonObject.getLong("id");
+            tweet.createdAt = jsonObject.getString("created_at");
+            tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Return the tweet object
+        return tweet;
+    }
+
+    public static ArrayList<Tweet> fromJSONArray (JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        // Iterate the json array and create tweets
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject tweetJson = jsonArray.getJSONObject(i);
+                Tweet tweet = Tweet.fromJSON(tweetJson);
+                if (tweet != null) {
+                    tweets.add(tweet);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        // Return the finished list
+        return tweets;
+    }
+}
