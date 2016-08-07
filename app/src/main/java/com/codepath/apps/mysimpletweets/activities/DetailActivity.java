3b27e51995a0a7c1aae66157e3ed9c038163344c
zhiyong tan <zhiyong@uber.com>
2016-08-06 20:52:27 -0700

    started detail

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/activities/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.apps.mysimpletweets.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codepath.apps.mysimpletweets.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
