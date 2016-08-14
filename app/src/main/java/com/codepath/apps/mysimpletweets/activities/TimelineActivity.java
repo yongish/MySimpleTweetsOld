f1a97b010829b61dcc8f7055954df23d0c068bfa
zhiyong tan <zhiyong@uber.com>
2016-08-13 18:38:41 -0700

    partway through video

--- app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
@@ -2,42 +2,30 @@ package com.codepath.apps.mysimpletweets.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.activeandroid.ActiveAndroid;
-import com.activeandroid.Configuration;
 import com.activeandroid.query.Select;
 import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.TwitterApplication;
 import com.codepath.apps.mysimpletweets.TwitterClient;
-import com.codepath.apps.mysimpletweets.adapters.TweetsArrayAdapter;
-import com.codepath.apps.mysimpletweets.models.Media;
+import com.codepath.apps.mysimpletweets.fragments.TweetsListFragment;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.codepath.apps.mysimpletweets.models.User;
-import com.codepath.apps.mysimpletweets.utils.EndlessRecyclerViewScrollListener;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
-
+    private TweetsListFragment fragmentTweetsList;
     private TwitterClient client;
-    private ArrayList<Tweet> tweets;
-    private TweetsArrayAdapter aTweets;
-    private SwipeRefreshLayout swipeContainer;
     private long lowestUid;
 
     @Override
@@ -45,49 +33,15 @@ public class TimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        tweets = new ArrayList<>();
-        RecyclerView rvTweets = (RecyclerView) findViewById(R.id.rvTweets);
-
-        aTweets = new TweetsArrayAdapter(this, tweets);
-        rvTweets.setAdapter(aTweets);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        rvTweets.setLayoutManager(linearLayoutManager);
-
-        Configuration.Builder config = new Configuration.Builder(this);
-        config.addModelClasses(Tweet.class, User.class, Media.class);
-        ActiveAndroid.initialize(this);
-
-        // Query ActiveAndroid for list of data and load result back to adapter user addAll
-        List<Tweet> queryResults = new Select().from(Tweet.class).orderBy("created_at DESC").limit(100).execute();
-        tweets.addAll(queryResults);
-        aTweets.notifyDataSetChanged();
-
-
         lowestUid = Long.MAX_VALUE;
+
         client = TwitterApplication.getRestClient();    // singleton client
         populateTimeline();
 
-
-        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount) {
-                // Triggered only when new data needs to be appended to the list
-                // Add whatever code is needed to append new items to the bottom of the list
-                populateTimeline();
-            }
-        });
-
-        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                populateTimeline();
-            }
-        });
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
+        if (savedInstanceState == null) {
+            // access the fragment
+            fragmentTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
+        }
     }
 
     @Override
@@ -108,12 +62,14 @@ public class TimelineActivity extends AppCompatActivity {
                 // CREATE MODELS AND ADD THEM TO THE ADAPTER
                 // LOAD THE MODEL DATA INTO LISTVIEW
                 //aTweets.addAll(Tweet.fromJSONArray(json));
-                aTweets.clear();
+
+//                aTweets.clear();
                 ArrayList<Tweet> newTweets = Tweet.fromJSONArray(json);
-                lowestUid = getLowestUid(newTweets);
-                tweets.addAll(newTweets);
-                aTweets.notifyDataSetChanged(); // TODO: Find out how many tweets are fetched. Avoid using notifyDataSetChanged().
-                swipeContainer.setRefreshing(false);
+//                lowestUid = getLowestUid(newTweets);
+//                tweets.addAll(newTweets);
+                fragmentTweetsList.addAll(newTweets);
+//                aTweets.notifyDataSetChanged(); // TODO: Find out how many tweets are fetched. Avoid using notifyDataSetChanged().
+//                swipeContainer.setRefreshing(false);
 
                 // Add to database.
                 for (Tweet tweet: newTweets) {
@@ -137,18 +93,8 @@ public class TimelineActivity extends AppCompatActivity {
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 String errString = "";
                 if (errorResponse != null) errString = errorResponse.toString();
-                Toast.makeText(getBaseContext(), errString, Toast.LENGTH_LONG).show();
             }
         }, lowestUid);
-    }
-
-    private long getLowestUid(ArrayList<Tweet> tweets) {
-        long lowestUid = Long.MAX_VALUE;
-        for (Tweet tweet: tweets) {
-            long uid = tweet.getUid();
-            if (uid < lowestUid) lowestUid = uid;
-        }
-        return lowestUid;
     }
 
     private final int REQUEST_CODE = 20;
@@ -160,10 +106,10 @@ public class TimelineActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
             String tweetBody = data.getExtras().getString("tweetBody");
-            getOwnUserDetails(tweetBody);
+//            getOwnUserDetails(tweetBody);
         }
     }
-
+/*
     private User self;
     public void getOwnUserDetails(final String tweetBody) {
         client.getUser(new JsonHttpResponseHandler() {
@@ -180,6 +126,6 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.d("FAILED GET USER DETAILS", errorResponse.toString());
             }
         });
-    }
+    }*/
 
 }
---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetsListFragment.java
@@ -0,0 +1,100 @@
+package com.codepath.apps.mysimpletweets.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Configuration;
+import com.activeandroid.query.Select;
+import com.codepath.apps.mysimpletweets.R;
+import com.codepath.apps.mysimpletweets.adapters.TweetsArrayAdapter;
+import com.codepath.apps.mysimpletweets.models.Media;
+import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.codepath.apps.mysimpletweets.models.User;
+import com.codepath.apps.mysimpletweets.utils.EndlessRecyclerViewScrollListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TweetsListFragment extends Fragment {
+
+    private ArrayList<Tweet> tweets;
+    private TweetsArrayAdapter aTweets;
+    private RecyclerView rvTweets;
+    private SwipeRefreshLayout swipeContainer;
+
+    // inflation logic
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tweets_list, container, false);
+
+        rvTweets = (RecyclerView) v.findViewById(R.id.rvTweets);
+        aTweets = new TweetsArrayAdapter(getActivity(), tweets);
+        rvTweets.setAdapter(aTweets);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        rvTweets.setLayoutManager(linearLayoutManager);
+        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+//                populateTimeline();
+            }
+        });
+
+        swipeContainer = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+//                populateTimeline();
+
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        return v;
+    }
+
+    // creation lifecycle event
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tweets = new ArrayList<>();
+
+        Configuration.Builder config = new Configuration.Builder(getActivity());
+        config.addModelClasses(Tweet.class, User.class, Media.class);
+        ActiveAndroid.initialize(getActivity());
+
+        // Query ActiveAndroid for list of data and load result back to adapter user addAll
+        List<Tweet> queryResults = new Select().from(Tweet.class).orderBy("created_at DESC").limit(100).execute();
+        tweets.addAll(queryResults);
+
+//        aTweets.notifyDataSetChanged();
+    }
+
+    public void addAll(List<Tweet> tweets) {
+        aTweets.addAll(tweets);
+    }
+
+    private long getLowestUid(ArrayList<Tweet> tweets) {
+        long lowestUid = Long.MAX_VALUE;
+        for (Tweet tweet: tweets) {
+            long uid = tweet.getUid();
+            if (uid < lowestUid) lowestUid = uid;
+        }
+        return lowestUid;
+    }
+}
